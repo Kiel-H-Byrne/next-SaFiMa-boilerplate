599d195f718c476ac7b49b61c2841d80838e09c6
Kiel <kiel.byrne@gmail.com>
2019-08-30 00:39:54 -0400

    pruning

--- src/Components/Assets/AssetSearch.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const AssetSearch = props => <div ></div>
-
-export default AssetSearch
--- src/Components/HOCs/MemoizedComponent.js
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-
-// const function ChildComponent(props) {
-//     return (
-//               {children}
-//             );
-//           }
-
-// export const MemoizedComponent = React.memo(ChildComponent);
